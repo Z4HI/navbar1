b32859a826b1278e711413d53a1c30e6ceb0ccd6
Zahi <102051293+Z4HI@users.noreply.github.com>
2023-11-28 20:07:18 -0500

    Added javascript toggle on select value selection

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 console.log('hellooo');
 
 const teamoption = document.querySelector('#teamoption');
-const teamselected = document.querySelector('#teamselected');
+const teamselected = document.querySelector('.teamselected');
 
 teamoption.addEventListener("change", setTeam);
 
@@ -10,14 +10,23 @@ function setTeam(){
     const choice = teamoption.value;
 
     if(choice==="heat"){
-        teamselected.textContent = "You selected the miami heat!"
+        teamselected.textContent = "IDK how they are even good tbh"
+        teamselected.classList.toggle("teamentrance");
     }
 
     else if(choice==="suns"){
-        teamselected.textContent = "You selected the Phoenix Suns!"
+        teamselected.textContent = "KD and DBook are pretty good I guess"
+        teamselected.classList.toggle("teamentrance");
     }
 
     else if(choice==="nuggets"){
-        teamselected.textContent = "You selected the Denver Nuggets!"
+        teamselected.textContent = "Jokic is really good at basketball"
+        teamselected.classList.toggle("teamentrance");
+    }
+    else{
+        teamselected.textContent = "";
+        teamselected.classList.toggle("teamentrance");
     }
 }
+
+
